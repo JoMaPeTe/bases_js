a9259f89882c738a59eceba5f36b0f4445c975d4
JoseMPeTe <jmperezterrero@gmail.com>
2023-12-07 14:52:41 +0100

    s3/l9 import export funcions

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
 import {heroes} from './data/heroes'
-console.log(heroes)
+// console.log(heroes)
+
+// const getHeroeById = (id) =>{
+//     return heroes.find((heroe)=>heroe.id == id);
+// }
+
+const getHeroeById = (id) =>heroes.find((heroe)=>heroe.id == id);
+
+console.log(getHeroeById(2))
+
+const getHeroesByOwner = (owner) => heroes.filter((heroe)=>heroe.owner==owner)
+console.log(getHeroesByOwner('DC'));
